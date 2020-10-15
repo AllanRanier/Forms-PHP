9e772908734b941356d143967db43effdedffa9d
AllanRanier <56282833+AllanRanier@users.noreply.github.com>
2020-10-14 23:28:02 -0300

    update

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
+<?php
+
+$usuario = $_POST['usuario'];
+$senha = $_POST['senha'];
+
+if(isset($_POST['Entrar'])){
+  if($usuario == "admin" && $senha == "admin"){
+    header("Location: home.html");
+    exit();
+  }else{
+    //unset($_POST['Entrar']);
+    echo '<script>alert("Usuário e senha incorretos!")</script>';
+  }
+}
+
+?>
+
 <!doctype html>
-<html lang="en">
+<html lang="pt-br">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -10,21 +27,22 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
 
-    <title>Formulario de Login</title>
+    <title>Login</title>
   </head>
   <body> 
     <div id="central-login">
     <div class="content" align="center">
-        <form method="POST" class="col-sm-4">
+        <form method="POST" class="col-sm-5">
+            <!-- Input do Usuario -->
             <div class="input-container">
                 <i class="fa fa-user icon"></i>
-                    <input type="text" name="usuario" class="form-control" placeholder="Usuário"><br>
+                <input type="text" name="usuario" class="form-control" placeholder="Usuário" require><br>
             </div>
+            <!-- Input da Senha -->
             <div class="input-container">
                 <i class="fa fa-key icon"></i>
-                    <input type="password" name="senha" class="form-control" placeholder="*********"><br>
+                <input type="password" name="senha" class="form-control" placeholder="Senha" require><br>
             </div>
-
             <input type="submit" name="Entrar" class="btn btn-outline-primary btn-lg btn-block">
         </form>
     </div>
@@ -34,3 +52,5 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
   </body>
 </html>
+
+
